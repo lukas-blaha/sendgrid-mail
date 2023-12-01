f4c1dcaee0d2272e6891b67f40568b5ddfdc68c1
Lukas Blaha <lukas.blaha@tietoevry.com>
2023-12-01 09:23:10 +0100

    Add parameters into message

--- cmd/main.go
+++ cmd/main.go
@@ -1,40 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	d := NewData()
 
-	fmt.Println(d)
+	msg := NewMsgBody(d.To, d.From, d.Subject, d.Body)
 
-	// msg := NewMsgBody([]string{"mail@example.com"}, "test@example.com", "Test subject", "Test body")
-	//
-	// j, err := json.Marshal(msg)
-	//
-	//	if err != nil {
-	//		log.Panic(err)
-	//	}
-	//
-	// body := bytes.NewBuffer(j)
-	//
-	// client := &http.Client{}
-	//
-	// r, err := http.NewRequest(http.MethodPost, "https://api.sendgrid.com/v3/mail/send", body)
-	//
-	//	if err != nil {
-	//		log.Panic(err)
-	//	}
-	//
-	// r.Header.Set("Authorization", "Bearer")
-	// r.Header.Set("Content-Type", "application/json")
-	//
-	// resp, err := client.Do(r)
-	//
-	//	if err != nil {
-	//		log.Panic(err)
-	//	}
-	//
-	// defer resp.Body.Close()
-	//
-	// fmt.Println(resp.Status)
+	j, err := json.Marshal(msg)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	body := bytes.NewBuffer(j)
+
+	client := &http.Client{}
+
+	r, err := http.NewRequest(http.MethodPost, "https://api.sendgrid.com/v3/mail/send", body)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	r.Header.Set("Authorization", "Bearer")
+	r.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(r)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Status)
 }
